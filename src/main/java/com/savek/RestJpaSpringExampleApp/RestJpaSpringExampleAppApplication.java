cdc8411236dce87519ef5daafd038f0a870c7e1e
av.sorokin <A.Sorokin@mils-s.ru>
2019-12-09 22:29:40 +0500

    Добавлен скелет проекта

---
+++ src/main/java/com/savek/RestJpaSpringExampleApp/RestJpaSpringExampleAppApplication.java
@@ -0,0 +1,13 @@
+package com.savek.RestJpaSpringExampleApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestJpaSpringExampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestJpaSpringExampleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/savek/RestJpaSpringExampleApp/RestJpaSpringExampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savek.RestJpaSpringExampleApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestJpaSpringExampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
